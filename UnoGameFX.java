0bbc75b85bd115ea9f4c7a9deae05d1f2700f07a
Yohana701 <105068132+Yohana701@users.noreply.github.com>
2024-08-15 19:26:00 -0700

    Create UnoGameFX.java

--- UnoGameFX.java
+++ UnoGameFX.java
@@ -1,1 +1,24 @@
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
 
+public class UnoGameFX extends Application {
+    @Override
+    public void start(Stage primaryStage) {
+        primaryStage.setTitle("UNO Game with JavaFX");
+
+        Label label = new Label("JavaFX integration example");
+
+        VBox vbox = new VBox(label);
+        Scene scene = new Scene(vbox, 300, 200);
+
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
